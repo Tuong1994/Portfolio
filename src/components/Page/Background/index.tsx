39b39e5bc77cf0f295410d9423938d5123105120
unknown <nhambontuong68@gmail.com>
2024-04-30 21:36:06 +0700

    update background section

--- src/components/Page/Background/index.tsx
+++ src/components/Page/Background/index.tsx
@@ -1,9 +1,33 @@
 import { FC } from "react";
+import { UI } from "@/components";
+import { HiHeart } from "react-icons/hi2";
+import useThemeStore from "@/store/ThemeStore";
+import utils from "@/utils";
+
+const { Typography, Space } = UI;
+
+const { Paragraph } = Typography;
 
 interface BackgroundProps {}
 
 const Background: FC<BackgroundProps> = () => {
-  return <div className="background" />;
+  const color = useThemeStore((state) => state.color);
+
+  const colorClassName = `background-${color}`;
+
+  const mainClassName = utils.formatClassName("background", colorClassName);
+
+  return (
+    <div className={mainClassName}>
+      <Space align="middle" justify="center" rootClassName="background-content">
+        <Paragraph size={40}>I</Paragraph>
+        <HiHeart size={40} />
+        <Paragraph size={60} weight={600} rootClassName="content-mark">
+          Coding
+        </Paragraph>
+      </Space>
+    </div>
+  );
 };
 
 export default Background;
